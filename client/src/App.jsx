dd0ff4fb8008da109cac597b480838e5ef5b4d99
OrTab <or.1532.tabin@gmail.com>
2024-01-21 00:07:36 +0200

    add guards component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,60 @@
-import { useEffect } from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import "./assets/styles/main.scss";
-import { Login } from "./cmps/Login";
-import { SignUp } from "./cmps/SignUp";
-import { BoardApp } from "./pages/BoardApp";
-import { Home } from "./pages/Home";
-import { UserProfile } from "./pages/user/UserProfile";
-import { socketService } from "./services/socketService";
+import { useEffect } from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import './assets/styles/main.scss';
+import { Login } from './cmps/Login';
+import { SignUp } from './cmps/SignUp';
+import { BoardApp } from './pages/BoardApp';
+import { Home } from './pages/Home';
+import { UserProfile } from './pages/user/UserProfile';
+import { socketService } from './services/socketService';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { NON_PRODUCT_ROUTES } from './constants';
 
+const isInProduct = () =>
+    !NON_PRODUCT_ROUTES.includes(window.location.pathname);
+
+const Guards = () => {
+    const { loggedInUser } = useSelector(state => state.userReducer);
+    const history = useHistory();
+
+    useEffect(() => {
+        if (loggedInUser) {
+            if (!isInProduct()) {
+                history.push('/board');
+            }
+        } else {
+            if (isInProduct()) {
+                history.push('/');
+            }
+        }
+        return () => {
+            socketService.terminate();
+        };
+    }, [history, loggedInUser]);
+
+    useEffect(() => {
+        return () => {
+            socketService.terminate();
+        };
+    }, []);
+
+    return null;
+};
 export function App() {
-  useEffect(() => {
-    return () => {
-      socketService.terminate();
-    };
-  }, []);
-  return (
-    <div className='App'>
-      <Router>
-        <Switch>
-          <Route path='/board/:boardId' component={BoardApp} />
-          <Route path='/user/:userId' component={UserProfile} />
-          <Route path='/board' component={BoardApp} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signUp' component={SignUp} />
-          <Route exact path='/' component={Home} />
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Guards />
+                <Switch>
+                    <Route path="/board/:boardId" component={BoardApp} />
+                    <Route path="/user/:userId" component={UserProfile} />
+                    <Route path="/board" component={BoardApp} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signUp" component={SignUp} />
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
